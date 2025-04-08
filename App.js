fb1f9c6ab96636e40b80fc28e7b8c313f06ea985
jaya kumari <jaya.kumari@mirumagency.com>
2025-04-08 23:41:39 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement(
-    "h1",
-     {id:"heading"}, 
-     "Hello World from React"
-    );
+const Title = ()=>{
+    return React.createElement(
+        "h1",
+        {id:"heading"}, 
+        "Hello World from React"
+    )
+}
+
+
+const HeadingComponent = () => (
+    <div className="container">
+        <Title></Title>
+        <h1 id="heading">Component composition</h1>
+    </div>
+)
+ 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
